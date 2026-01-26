6e63f315a11557d421968d2cd4a8a353b05518d1
Triktor <yosoyvictor06@gmail.com>
2026-01-26 13:44:35 +0100

    apartado 9 pero no peta???
    víctor necesito que lo testees porque a mí me va bien siempre

--- IG1App/RGBRectangle.cpp
+++ IG1App/RGBRectangle.cpp
@@ -10,9 +10,24 @@ void RGBRectangle::render(const glm::mat4& modelViewMat) const {
 	if (mMesh != nullptr) {
 		glm::mat4 aMat = modelViewMat * mModelMat; // glm matrix multiplication
 		mShader->use();
+		
+		//Pongo esto porque da error en algunos dispositivos
+		glEnable(GL_CULL_FACE);
+		
+		//Defino el método de render por delante y renderizo
+		glCullFace(GL_FRONT);
 		glPolygonMode(GL_FRONT, GL_LINE);
-		glPolygonMode(GL_BACK, GL_FILL);
-		upload(aMat);
 		mMesh->render();
+
+		//Defino el método de render por detrás y renderizo
+		glCullFace(GL_BACK);
+		glPolygonMode(GL_BACK, GL_FILL);
+		mMesh->render();
+
+		//Lo dejo como estaba antes para no afectar a otros objetos
+		glDisable(GL_CULL_FACE);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+		upload(aMat);
 	}
 }
