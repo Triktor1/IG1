a7c39d063f800fd1dac25660e3a5048fe02b8cee
Triktor <yosoyvictor06@gmail.com>
2026-02-06 16:47:18 +0100

    APARTADO 17 RRRRAAAAAAH

--- IG1App/RGBCube.cpp
+++ IG1App/RGBCube.cpp
@@ -4,4 +4,26 @@
 RGBCube::RGBCube(GLdouble l) : EntityWithColors()
 {
 	mMesh = Mesh::generateRGBCubeTriangles(l);
+	mModelMat = glm::translate(mModelMat, glm::vec3(l/2, l/2, -l/2));
+}
+
+void RGBCube::update() {
+	alphaAct += alpha;
+
+	switch (eje) {
+	case 0:
+		mModelMat = glm::rotate(glm::mat4(1), glm::radians(alpha), glm::vec3(1.0, 0.0, 0.0)) * mModelMat;
+		break;
+	case 1:
+		mModelMat = glm::rotate(glm::mat4(1), glm::radians(alpha), glm::vec3(0.0, 0.0, 1.0)) * mModelMat;
+		break;
+	case 2:
+		mModelMat = glm::rotate(glm::mat4(1), glm::radians(alpha), glm::vec3(0.0, 1.0, 0.0)) * mModelMat;
+		break;
+	}
+
+	if (alphaAct >= 180) {
+		alphaAct = 0;
+		eje = (eje+1) % 3;
+	}
 }
--- IG1App/RGBCube.h
+++ IG1App/RGBCube.h
@@ -3,6 +3,10 @@
 class RGBCube :
     public EntityWithColors
 {
+private:
+    GLfloat alpha = 2.0f, alphaAct = 0.0f;
+    GLint eje = 0;
 public:
     RGBCube(GLdouble l);
+    void update();
 };
