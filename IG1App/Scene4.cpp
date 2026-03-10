8c03d90e799ac780df552d099c295275ab2563a5
Triktor <yosoyvictor06@gmail.com>
2026-03-10 22:40:38 +0100

    limpia de escena

--- IG1App/Scene4.cpp
+++ IG1App/Scene4.cpp
@@ -5,58 +5,71 @@ using namespace glm;
 void
 Scene4::init()
 {
-	//Variables del RGBCube
-	GLdouble l = 200.0;
 
 	Scene::init();
 
 	// GROUND
+	//Variables del suelo
+	GLdouble groundL = 400.0;
 	Texture* tex = new Texture();
 	tex->load("../assets/images/baldosaC.png", 255);
 	gTextures.push_back(tex);
-	gObjects.push_back(new Ground(l * 2, l * 2, 4, 4, tex));
 
-	glm::mat4 groundRot = {
-		{1, 0, 0, 0},
-		{0, 0, -1, 0}, //0, cos(90), -sin(90), 0
-		{0, 1, 0, 0}, //0, sin(90), cos(90), 0
-		{0, 0, 0, 1}
-	};
-	gObjects.back()->setModelMat(groundRot);
+	Ground* ground = new Ground(groundL, groundL, 4, 4, tex);
+	gObjects.push_back(ground);
+	//Rotado 90º para que esté paralelo al plano XZ
+	ground->setModelMat(glm::rotate(ground->modelMat(), glm::radians(90.0f), glm::vec3(1, 0, 0)));
 
 	// BOX
+	//Variables de la caja
+	GLfloat boxPosX = 140.0f, boxPosY = 25.1f, boxPosZ = 0.0f;
+	GLuint boxL = 50;
 	Texture* texBox1 = new Texture();
 	Texture* texBox2 = new Texture();
 	texBox1->load("../assets/images/papelE.png", 255);
 	texBox2->load("../assets/images/container.jpg", 255);
 	gTextures.push_back(texBox1);
 	gTextures.push_back(texBox2);
-	Box* box = new Box(50, texBox1, texBox2);
+
+	Box* box = new Box(boxL, texBox1, texBox2);
 	gObjects.push_back(box);
-	box->setModelMat(glm::translate(box->modelMat(), glm::vec3(140.0, 25.0, 0.0)));
+	//Movimiento de la caja para ponerla en la posición correcta
+	box->setModelMat(glm::translate(box->modelMat(), glm::vec3(boxPosX, boxPosY, boxPosZ)));
 
 	// STAR
+	//Variables de la estrella
+	GLfloat starPosX = 140.0f, starPosY = 25.1f, starPosZ = 0.0f;
+	GLuint starRe = 17, starNp = 8, starH = 17;
 	Texture* texS = new Texture();
 	texS->load("../assets/images/rueda.png", 255);
 	gTextures.push_back(texS);
-	Star3D* star = new Star3D(17, 8, 17, texS);
+
+	Star3D* star = new Star3D(starRe, starNp, starH, texS);
 	gObjects.push_back(star);
-	star->setModelMat(glm::translate(star->modelMat(), glm::vec3(140.0, 25.1, 0.0)));
+	//Movimiento de la estrella para ponerla en la posición correcta
+	star->setModelMat(glm::translate(star->modelMat(), glm::vec3(starPosX, starPosY, starPosZ)));
 
 	// PHOTO
+	//Variables de la foto
+	GLuint photoL = 100;
 	Texture* empty = new Texture();
 	gTextures.push_back(empty);
-	Photo* photo = new Photo(100, 100, empty);
-	gObjects.push_back(photo);
 	
-	gObjects.back()->setModelMat(groundRot);
-	photo->setModelMat(glm::translate(photo->modelMat(), glm::vec3(0, 0, 0.1)));
+	Photo* photo = new Photo(photoL, photoL, empty);
+	gObjects.push_back(photo);
+	//Rotación para estar paralelo al suelo y subida para evitar el z-fighting con el suelo
+	photo->setModelMat(glm::rotate(photo->modelMat(), glm::radians(90.0f), glm::vec3(1, 0, 0)) *
+		glm::translate(photo->modelMat(), glm::vec3(0.0f, 0.0f, -0.1f)));
 
 	// GLASSPARAPET
+	//Variables del glass parapet
+	GLuint glassL = 400;
+	GLfloat glassScaleXZ = 1.0f, glassScaleY = 0.3f;
+
 	Texture* texGlass = new Texture();
 	texGlass->load("../assets/images/windowC.png", 255);
 	gTextures.push_back(texGlass);
-	GlassParapet* glass = new GlassParapet(400, texGlass);
+	GlassParapet* glass = new GlassParapet(glassL, texGlass);
 	gObjects.push_back(glass);
-	glass->setModelMat(glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 0.3f, 1.0f)));
+	glass->setModelMat(glm::scale(glm::mat4(1.0f), glm::vec3(glassScaleXZ, glassScaleY, glassScaleXZ)));
 }
