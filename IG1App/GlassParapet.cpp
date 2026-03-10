34ac2bf04448be32d1ada621e6356c1be0e78db6
Triktor <yosoyvictor06@gmail.com>
2026-03-10 16:51:00 +0100

    apartado 32 terminado

--- IG1App/GlassParapet.cpp
+++ IG1App/GlassParapet.cpp
@@ -10,13 +10,21 @@ void GlassParapet::render(const glm::mat4& modelViewMat) const {
 	if (mMesh != nullptr) {
 		glm::mat4 aMat = modelViewMat * mModelMat;
 		mShader->use();
-
-		//mShader->setUniform("modulate", mModulate);
 		mShader->setUniform("texture", mModulate);
+
+		glDepthMask(GL_FALSE);
+		glEnable(GL_BLEND);
+
+		glBlendFunc(GL_CONSTANT_ALPHA, GL_ONE_MINUS_CONSTANT_ALPHA);
+		glBlendColor(1.0f, 1.0f, 1.0f, 0.5f);
 
 		if (mTexture != nullptr) mTexture->bind();
 		upload(aMat);
 		mMesh->render();
 		if (mTexture != nullptr) mTexture->unbind();
+
+		glBlendColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glEnable(GL_BLEND);
+		glDepthMask(GL_TRUE);
 	}
 }
