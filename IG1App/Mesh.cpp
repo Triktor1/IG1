70b0f1c17e644752d4325acf8ed4953c8a34e3fb
Triktor <yosoyvictor06@gmail.com>
2026-04-17 17:52:30 +0200

    apartado cincuenta y siete

--- IG1App/Mesh.cpp
+++ IG1App/Mesh.cpp
@@ -63,6 +63,17 @@ Mesh::load()
 				sizeof(vec2), nullptr);
 			glEnableVertexAttribArray(2);
 		}
+
+		if (vNormals.size() > 0) {
+			glGenBuffers(1, &mNBO);
+			glBindBuffer(GL_ARRAY_BUFFER, mNBO);
+			glBufferData(GL_ARRAY_BUFFER,
+				vNormals.size() * sizeof(vec3),
+				vNormals.data(), GL_STATIC_DRAW);
+			glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE,
+				sizeof(vec3), nullptr);
+			glEnableVertexAttribArray(3);
+		}
 	}
 }
 
@@ -83,6 +94,11 @@ Mesh::unload()
 		if (mTCO != NONE) {
 			glDeleteBuffers(1, &mTCO);
 			mTCO = NONE;
+		}
+
+		if (mNBO != NONE) {
+			glDeleteBuffers(1, &mNBO);
+			mNBO = NONE;
 		}
 	}
 }
--- IG1App/Mesh.h
+++ IG1App/Mesh.h
@@ -45,6 +45,7 @@ protected:
 	std::vector<glm::vec3> vVertices; // vertex array
 	std::vector<glm::vec4> vColors;   // color array
 	std::vector<glm::vec2> vTexCoords; // texture coordinates array
+	std::vector<glm::vec3> vNormals; //normals array
 
 	GLuint mVAO;  // vertex array object
 
@@ -52,6 +53,7 @@ private:
 	GLuint mVBO;  // vertex buffer object
 	GLuint mCBO;  // color buffer object
 	GLuint mTCO;  // texture coordinates buffer object
+	GLuint mNBO;  // normals buffer object
 };
 
 #endif //_H_Scene_H_
