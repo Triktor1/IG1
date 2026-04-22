7ae211619ce7e38580751632044faec309ceb4da
Triktor <yosoyvictor06@gmail.com>
2026-04-22 18:18:20 +0200

    ejemplo pa ti victor

--- IG1App/Scene9.cpp
+++ IG1App/Scene9.cpp
@@ -4,5 +4,8 @@ void
 Scene9::init() {
 	Scene::init();
 	Snowman* snowman = new Snowman(100);
+	//Ejemplo para mover al CompoundEntities
+	//glm::mat4 snowmanMat = snowman->modelMat() * glm::translate(glm::mat4(1), glm::vec3(100,0,0));
+	//snowman->setModelMat(snowmanMat);
 	gOpaqueObjects.push_back(snowman);
 }
