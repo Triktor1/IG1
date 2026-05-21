93986c65d563e1b4abb8b8f96f56856bf7dc23e1
Triktor <yosoyvictor06@gmail.com>
2026-05-21 18:34:34 +0200

    normales bien

--- IG1App/EntityWithMaterial.cpp
+++ IG1App/EntityWithMaterial.cpp
@@ -1,24 +1,27 @@
 #include "EntityWithMaterial.h"
 
 EntityWithMaterial::EntityWithMaterial() {
-    mShader = Shader::get("light");
+	mShader = Shader::get("light");
 }
 
 void
 EntityWithMaterial::render(const glm::mat4& modelViewMat) const
 {
-    mShader->use();
-    // Carga los atributos del material en la GPU
-    material.upload(*mShader);
-    upload(modelViewMat * mModelMat);
-    mMesh->render();
+	if (mMesh != nullptr) {
 
-    if (mShowNormals) {
-        Shader* shader = Shader::get("normals");
-        shader->use();
-        shader->setUniform("modelViewMat", modelViewMat * mModelMat);
-        mMesh->render();
-    }
+		mShader->use();
+		// Carga los atributos del material en la GPU
+		material.upload(*mShader);
+		upload(modelViewMat * mModelMat);
+		mMesh->render();
+
+		if (mShowNormals) {
+			Shader* shader = Shader::get("normals");
+			shader->use();
+			shader->setUniform("modelView", modelViewMat * mModelMat);
+			mMesh->render();
+		}
+	}
 }
-EntityWithMaterial::~EntityWithMaterial(){
+EntityWithMaterial::~EntityWithMaterial() {
 }
