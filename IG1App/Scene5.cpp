df32e076c5b7f1bd45dade1489cf48a89b7bb090
Triktor <yosoyvictor06@gmail.com>
2026-05-22 19:10:36 +0200

    torus partible en horizontal y vertical

--- IG1App/Scene5.cpp
+++ IG1App/Scene5.cpp
@@ -11,7 +11,10 @@ Scene5::init()
 	GLdouble torus_R = 80, torus_r = 40;
 
 	//Torus
-	Torus* torus = new Torus(torus_R, torus_r);
+	Torus* torus = new Torus(torus_R, torus_r, 40, 40, std::numbers::pi, std::numbers::pi);
 	torus->setColor(vec4(0, 1, 0, 1));
 	gOpaqueObjects.push_back(torus);
+	//Cone* cone = new Cone(100, torus_r, torus_R, 20, 4, std::numbers::pi * 2);
+	//cone->setColor(vec4(0, 1, 0, 1));
+	//gOpaqueObjects.push_back(cone);
 }
--- IG1App/Torus.h
+++ IG1App/Torus.h
@@ -5,5 +5,5 @@ class Torus :
 	public ColorMaterialEntity
 {
 public:
-	Torus(GLdouble R, GLdouble r, GLuint nPoints = 40, GLuint nSamples = 40, GLfloat angleMax = 2 * std::numbers::pi);
+	Torus(GLdouble R, GLdouble r, GLuint nPoints = 40, GLuint nSamples = 40, GLfloat angleMax = 2 * std::numbers::pi, GLfloat angleMaxPoints = 2 * std::numbers::pi);
 };
