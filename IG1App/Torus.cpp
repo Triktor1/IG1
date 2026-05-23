564b7ca5091618fbb2a5c8d90fbb378fd82ed18b
Triktor <yosoyvictor06@gmail.com>
2026-05-23 10:27:34 +0200

    arreglitos

--- IG1App/Torus.cpp
+++ IG1App/Torus.cpp
@@ -1,11 +1,13 @@
 #include "Torus.h"
 
-Torus::Torus(GLdouble R, GLdouble r, GLuint nPoints, GLuint nSamples, GLfloat angleMax) :
+Torus::Torus(GLdouble R, GLdouble r, GLuint nPoints, GLuint nSamples, GLfloat angleMax, GLfloat angleMaxPoints) :
 	ColorMaterialEntity()
 {
 	std::vector<glm::vec2> profile;
-	GLdouble alphaStep = 2 * std::numbers::pi / (float)nPoints;
-	GLdouble alpha = std::numbers::pi / 2;
+	//GLdouble alphaStep = 2 * std::numbers::pi / (float)nPoints;
+	//GLdouble alpha = std::numbers::pi / 2;
+	GLdouble alphaStep = angleMaxPoints / (float)nPoints;
+	GLdouble alpha = angleMaxPoints;
 	for (int i = 0; i <= nPoints; ++i) {
 		profile.emplace_back(R + r * cos(alphaStep * i - alpha), r * sin(alphaStep * i - alpha));
 	}
