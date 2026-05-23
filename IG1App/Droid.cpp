a9d4bd266281ac9ec9daa66f8cd4728af55068e5
Triktor <yosoyvictor06@gmail.com>
2026-05-23 14:09:40 +0200

    droide detonado cataplum

--- IG1App/Droid.cpp
+++ IG1App/Droid.cpp
@@ -6,53 +6,7 @@
 #include "Light.h"
 
 Droid::Droid(GLdouble radius) {
-	//HEAD
-	head = new CompoundEntity();
-	addEntity(head);
-
-	//ESFERA
-	tx = new Texture();
-	tx->load("../assets/images/container.jpg", 255);
-	sphere = new SphereWithTexture(radius, 20, 20, tx);
-	addEntity(sphere);
-	head->addTexture(tx);
-
-	//SOMBRERO
-	hat = new Cone(radius / 2, radius / 2, radius, 10, 30);
-	glm::mat4 hatCilinderMat = glm::translate(glm::mat4(1), glm::vec3(0, radius, 0));
-	hat->setModelMat(hatCilinderMat);
-	hat->setColor(glm::vec4(1, 1, 0, 1));
-	head->addEntity(hat);
-
-	//DISCO
-	hatDisk = new Disk(radius * 0.55f, 0, 10, 30);
-	glm::mat4 diskCilinderMat = glm::translate(glm::mat4(1), glm::vec3(0, radius * 1.45f, 0));
-	hatDisk->setModelMat(diskCilinderMat);
-	hatDisk->setColor(glm::vec4(2, 2, 0, 1));
-	head->addEntity(hatDisk);
-
-	//OJOS
-	eye1 = new Cone(radius / 2, radius / 10, radius / 10, 10, 10);
-	glm::mat4 eye1CilinderMat = glm::translate(glm::mat4(1.0), glm::vec3(radius / 10, radius * 1.25, radius / 1.5)) *
-	glm::rotate(glm::mat4(1.0), glm::radians(90.0f), glm::vec3(1, 0, 0));
-	eye1->setModelMat(eye1CilinderMat);
-	eye1->setColor(glm::vec4(0, 0.8, 0, 1));
-	head->addEntity(eye1);
-
-	eye2 = new Cone(radius / 2, radius / 10, radius / 10, 10, 10);
-	glm::mat4 eye2CilinderMat = glm::translate(glm::mat4(1.0), glm::vec3(-radius / 10, radius * 1.25, radius / 1.5)) *
-	glm::rotate(glm::mat4(1.0), glm::radians(90.0f), glm::vec3(1, 0, 0));
-	eye2->setModelMat(eye2CilinderMat);
-	eye2->setColor(glm::vec4(0, 0.8, 0, 1));
-	head->addEntity(eye2);
 	
-	//LUZ
-	droidLight = new SpotLight(glm::vec3(0, 0, 0), 1);
-	droidLight->setDirection(glm::vec3(0, -1, 0));
-	droidLight->setAmb(glm::vec3(0.05f));
-	droidLight->setSpec(glm::vec3(0.0f, 0.2f, 0.0f));
-	droidLight->setCutoff(35.0f, 35.0f);
-	head->addLight(droidLight);
 }
 
 void Droid:: rotateSphere(GLfloat factor) {
